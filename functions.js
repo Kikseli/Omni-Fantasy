61386eea286fef71f33a229527c98830a2c3f997
Kikseli <49708903+Kikseli@users.noreply.github.com>
2026-08-05 18:21:50 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
+document.querySelectorAll(".user-link").forEach(link => {
+  link.removeAttribute("title");
+});
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+
+  return (
+    `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ` +
+    `klo ${String(date.getHours()).padStart(2, "0")}:` +
+    `${String(date.getMinutes()).padStart(2, "0")}`
+  );
+}
+
 document.querySelectorAll("abbr.o-timestamp").forEach((el) => {
   const timestamp = Number(el.dataset.timestamp);
 
@@ -5,12 +19,5 @@ document.querySelectorAll("abbr.o-timestamp").forEach((el) => {
     return;
   }
 
-  const date = new Date(timestamp);
-
-  const text =
-    `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ` +
-    `klo ${String(date.getHours()).padStart(2, "0")}:` +
-    `${String(date.getMinutes()).padStart(2, "0")}`;
-
-  el.textContent = text;
+  el.textContent = formatTimestamp(timestamp);
 });
